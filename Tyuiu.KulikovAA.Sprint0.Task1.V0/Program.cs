2e0efb846635ced762658ca8ff02e135fe39d9c6
Tyuiu.KulikovAA <k.alexandr174@gmail.com>
2023-09-19 19:57:13 +0500

    Task1

---
+++ Tyuiu.KulikovAA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KulikovAA.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Alex";
+            int age = 18;
+            Console.WriteLine("Hello my name is " + name + " to me " + age + " year! ");
+
+            Console.ReadKey();
+        }
+    }
+}
